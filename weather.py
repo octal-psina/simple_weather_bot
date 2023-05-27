7bfe7d7ff04a09ff3eaaba9c89a7e5fefbf24efd
Jhon <aleksmironov891@gmail.com>
2023-05-28 02:31:42 +0500

    Can't remember what exactly I had done but nothing really important

--- weather.py
+++ weather.py
@@ -2,62 +2,50 @@ import json
 import requests
 import openweather_key
 
-#import telebot
 
 class Weather:
-  def __init__(self, city):
-    self.city = city
+    def __init__(self, city):
+        self.city = city
 
-  def get_lat_lon(self):
-    """Получение широты и долготы запрашиваемого города"""
-    url = f"http://api.openweathermap.org/geo/1.0/direct?q={self.city}&limit=1&appid={openweather_key.key}"
-    city_lat_lon = requests.get(url).json()
-    #обращение к API openweather получение json city lat, lon
-    # запись информации в файл json
-    #with open("city_lat_lon.json", "w") as f:
-    #result_lat_lon = json.dump(city_lat_lon, f)
-    return city_lat_lon
-  
-  def get_city_weather(self):
-    """Получение широты и долготы из файла city_lat_lon.json"""
-    try:
-      json_data = Weather.get_lat_lon(self)
-      lat = json_data[0]["lat"]
-      lon = json_data[0]["lon"]
-      url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={openweather_key.key}&units=metric"
-      city_weather = requests.get(url).json()
-    except (IndexError):
-      city_weather = False
-    return city_weather
-  
-  def show_city_weather(self):
-    json_data = Weather.get_city_weather(self)
+    def get_lat_lon(self):
+        """Получение широты и долготы запрашиваемого города"""
+        url = f"http://api.openweathermap.org/geo/1.0/direct?q={self.city}&limit=1&appid={openweather_key.key}"
+        city_lat_lon = requests.get(url).json()
+        # обращение к API openweather получение json city lat, lon
+        # запись информации в файл json
+        # with open("city_lat_lon.json", "w") as f:
+        #result_lat_lon = json.dump(city_lat_lon, f)
+        return city_lat_lon
 
-#    with open("weather.json", "w") as f:
-#        json.dump(json_data, f)
-    if json_data != False:
-      dict_weather_info = {} 
-      dict_weather_info['name_city'] = json_data["name"]
-      dict_weather_info['weather'] = json_data['weather'][0]["main"]
-      dict_weather_info['wearher_description'] = json_data['weather'][0]["description"]
-      dict_weather_info['temp'] = json_data['main']['temp']
-      dict_weather_info['humidity'] = json_data['main']['humidity']
-      dict_weather_info['wind'] = json_data['wind']['speed']
-      #weather_info = f"Город:{self.city}\nПогода: {weather}\nТемпература: {temp}C\nВлажность: {humidity}%\nСкорость ветра: {wind} м/c"
-      #print(f"Город:{self.city}\nПогода:{weather}\nТемпература:{temp}C\nВлажность:{humidity}%\nСкорость ветра:{wind} м/c")
-    else:
-      dict_weather_info = f"Извините нам не известен город:\n{self.city}"
-    return 	dict_weather_info  
-	  
+    def get_city_weather(self):
+        """Получение широты и долготы из файла city_lat_lon.json"""
+        try:
+            json_data = Weather.get_lat_lon(self)
+            lat = json_data[0]["lat"]
+            lon = json_data[0]["lon"]
+            url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={openweather_key.key}&units=metric"
+            city_weather = requests.get(url).json()
+        # Использую перехват всех ошибок чтобы избежать проблем при обращение к API
+        except:
+            city_weather = False
+        return city_weather
+
+    def show_city_weather(self):
+        json_data = Weather.get_city_weather(self)
+
+        if json_data != False:
+            dict_weather_info = {}
+            dict_weather_info['name_city'] = json_data["name"]
+            dict_weather_info['weather'] = json_data['weather'][0]["main"]
+            dict_weather_info['wearher_description'] = json_data['weather'][0]["description"]
+            dict_weather_info['temp'] = json_data['main']['temp']
+            dict_weather_info['humidity'] = json_data['main']['humidity']
+            dict_weather_info['wind'] = json_data['wind']['speed']
+
+        else:
+            dict_weather_info = f"Извините нам не известен город:\n{self.city}"
+        return dict_weather_info
+
 #weather_moscow = Weather("ташкент")
-#print(weather_moscow.show_city_weather())
+# print(weather_moscow.show_city_weather())
 
-
-# from urllib.request import urlopen
-# def get_ip_data(self):
-#     url = 'https://ifconfig.me/all.json'
-#     response = urlopen(url)
-#     return json.load(response)
-# ip = Weather.get_ip_data(self)
-#     print(ip["ip_addr"])
-#     your_ip = ip["ip_addr"]
--- weather_bot.py
+++ weather_bot.py
@@ -10,6 +10,7 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['start', 'help'])
 async def start_message(message: types.Message):
     """
@@ -17,38 +18,37 @@ async def start_message(message: types.Message):
     """
     await message.answer(text="Привет!\nЯ расскажу тебе о погоде!\nПросто введи название города.")
 
+
 @dp.message_handler()
 async def send_result(message: types.Message):
-  w = Weather(message.text)
-  weather_info = w.show_city_weather()
-  if type(weather_info) == dict:
-    if weather_info['weather'] == 'Clear':
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgUAAxkBAAEJBu9kZmhNxcfbjSN8Nkki1mENxI9obwACgQoAAuG8kQQb22sPffFyFy8E')
-    elif weather_info['weather'] == 'Thunderstorm':
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJButkZmgjq45pI4GUMI_BDpBIIPxMSAACLAEAAqZESAs1JufkLCR1wy8E')
-    elif weather_info['weather'] == 'Drizzle':
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJBulkZmgMfkL7mB-ic2XAVoMrSFQa7AACJAEAAqZESAsL5_Fz7_gGkC8E')
-    elif weather_info['weather'] == 'Rain':
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgUAAxkBAAEJB0FkZnDQBdEBHGcKf0VTyuhyCWcTLQACnQoAAuG8kQQQ5wWwukYO6y8E')
-    elif weather_info['weather'] == 'Snow':
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJBu1kZmgoZHbVna34EzaYhercwXIQBwACOAEAAqZESAvMWrzVi1b-6S8E')
-    elif weather_info['weather'] == 'Mist' or weather_info['weather'] == 'Haze' or weather_info['weather'] == 'Fog':
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJBvNkZmi5YtkHqWFor4BKyAwP2R2C8wACaQEAAqZESAt_3dxMvA3vCS8E')
-    elif weather_info['weather'] == 'Smoke' or weather_info['weather'] == 'Ash':
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJBxZkZm3r-6roGoe8aB2v5a3wSlaM_gACZwEAAqZESAuW7HRI30tNZS8E')
-    elif weather_info['weather'] == 'Sand' or weather_info['weather'] == 'Dust':
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJBwtkZm0vmLptf5N-pYkbUUy6ijAAARsAAjYBAAKmREgLC4izjG0lEKgvBA')
-    elif weather_info['weather'] == 'Squall' or weather_info['weather'] == 'Tornado':
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgUAAxkBAAEJBxlkZm4BGIr4J7VaJGaFDcldMSIc6wACoAoAAuG8kQTGGaKsxrKdiS8E')
-    elif weather_info['weather'] == 'Clouds':
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgUAAxkBAAEJBvVkZmlN-hDboU8uc5LBWiQz3f-QgwACngoAAuG8kQToqkk3_7S6eS8E')
-        
-    await message.reply(text = f"Город: {weather_info['name_city']}\nПогода: {weather_info['weather']}\nКраткое описание: {weather_info['wearher_description']}\nТемпература: {weather_info['temp']}C\nВлажность: {weather_info['humidity']}%\nСкорость ветра: {weather_info['wind']} м/c")
-  else:
-      await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJB0tkZnzXY2Jh5EglNU7cIG1qRDCLWQACJAADwZxgDEgkWrolDSiOLwQ')
-      await message.reply(text=weather_info)
-      
-#  await message.reply(text = f"Город: {weather_info['name_city']}\nПогода: {weather_info['weather']}\nКраткое описание: {weather_info['weather_description']}\nТемпература: {weather_info['temp']}C\nВлажность: {weather_info['humidity']}%\nСкорость ветра: {weather_info['wind']} м/c")
+    w = Weather(message.text)
+    weather_info = w.show_city_weather()
+    if type(weather_info) == dict:
+        if weather_info['weather'] == 'Clear':
+            await bot.send_sticker(message.from_user.id, sticker='CAACAgUAAxkBAAEJBu9kZmhNxcfbjSN8Nkki1mENxI9obwACgQoAAuG8kQQb22sPffFyFy8E')
+        elif weather_info['weather'] == 'Thunderstorm':
+            await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJButkZmgjq45pI4GUMI_BDpBIIPxMSAACLAEAAqZESAs1JufkLCR1wy8E')
+        elif weather_info['weather'] == 'Drizzle':
+            await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJBulkZmgMfkL7mB-ic2XAVoMrSFQa7AACJAEAAqZESAsL5_Fz7_gGkC8E')
+        elif weather_info['weather'] == 'Rain':
+            await bot.send_sticker(message.from_user.id, sticker='CAACAgUAAxkBAAEJB0FkZnDQBdEBHGcKf0VTyuhyCWcTLQACnQoAAuG8kQQQ5wWwukYO6y8E')
+        elif weather_info['weather'] == 'Snow':
+            await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJBu1kZmgoZHbVna34EzaYhercwXIQBwACOAEAAqZESAvMWrzVi1b-6S8E')
+        elif weather_info['weather'] == 'Mist' or weather_info['weather'] == 'Haze' or weather_info['weather'] == 'Fog':
+            await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJBvNkZmi5YtkHqWFor4BKyAwP2R2C8wACaQEAAqZESAt_3dxMvA3vCS8E')
+        elif weather_info['weather'] == 'Smoke' or weather_info['weather'] == 'Ash':
+            await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJBxZkZm3r-6roGoe8aB2v5a3wSlaM_gACZwEAAqZESAuW7HRI30tNZS8E')
+        elif weather_info['weather'] == 'Sand' or weather_info['weather'] == 'Dust':
+            await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJBwtkZm0vmLptf5N-pYkbUUy6ijAAARsAAjYBAAKmREgLC4izjG0lEKgvBA')
+        elif weather_info['weather'] == 'Squall' or weather_info['weather'] == 'Tornado':
+            await bot.send_sticker(message.from_user.id, sticker='CAACAgUAAxkBAAEJBxlkZm4BGIr4J7VaJGaFDcldMSIc6wACoAoAAuG8kQTGGaKsxrKdiS8E')
+        elif weather_info['weather'] == 'Clouds':
+            await bot.send_sticker(message.from_user.id, sticker='CAACAgUAAxkBAAEJBvVkZmlN-hDboU8uc5LBWiQz3f-QgwACngoAAuG8kQToqkk3_7S6eS8E')
+
+        await message.reply(text=f"Город: {weather_info['name_city']}\nПогода: {weather_info['weather']}\nКраткое описание: {weather_info['wearher_description']}\nТемпература: {weather_info['temp']}C\nВлажность: {weather_info['humidity']}%\nСкорость ветра: {weather_info['wind']} м/c")
+    else:
+        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEJB0tkZnzXY2Jh5EglNU7cIG1qRDCLWQACJAADwZxgDEgkWrolDSiOLwQ')
+        await message.reply(text=weather_info)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
